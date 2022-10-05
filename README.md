cf190d34bb06490c61a7fed4fe6a12a6c4003d1e
Eduardo Donato <eduardo.donato@gmail.com>
2022-10-04 22:41:52 -0300

    automatic renew screenshot option

--- README.md
+++ README.md
@@ -17,3 +17,15 @@ Get the current saved screenshot
 > GET /print
 
 Take a screenshot and save it
+
+## Configuration
+
+Configuration by environment variables:
+
+| Variable       | Meaning                              | Default                           |
+| -------------- | ------------------------------------ | --------------------------------- |
+| PORT           | Outgoing application port            | 3000                              |
+| URL            | Webpage to screenshot                | <https://www.github.com/ebdonato> |
+| AUTO_HOURS[^1] | Hours of day to renew the screenshot | 9 21                              |
+
+[^1]: Only natural integer number equal or greater then 0 and lower than 24
--- index.js
+++ index.js
@@ -1,24 +1,35 @@
+require("dotenv-defaults").config()
 const app = require("express")()
 const puppeteer = require("puppeteer")
+const cron = require("node-cron")
 const {accessSync, constants} = require("node:fs")
 
-const PORT = process.env.PORT || 3000
-const URL = process.env.URL || "https://www.github.com/ebdonato"
+const PORT = process.env.PORT
+const URL = process.env.URL
 
 const SCREENSHOT_PATH = "screenshot.png"
 
+const logger = {
+    log(...message) {
+        console.log(new Date().toString(), ...message)
+    },
+    error(...message) {
+        console.error(new Date().toString(), ...message)
+    },
+}
+
 async function printScreen() {
-    console.log("Obtendo screenshot...")
+    logger.log("Obtendo screenshot...")
 
     let browser = null
 
     try {
         browser = await puppeteer.launch({args: ["--no-sandbox"]})
         const page = await browser.newPage()
-        console.log(`Going to ${URL}`)
+        logger.log(`Going to ${URL}`)
         await page.goto(URL, {waitUntil: "networkidle0"})
         await page.screenshot({path: SCREENSHOT_PATH})
-        console.log("File saved")
+        logger.log("File saved")
     } catch (error) {
         throw error
     }
@@ -33,27 +44,27 @@ async function printScreen() {
 async function send() {}
 
 app.get("/print", (request, response) => {
-    console.log("Take screenshot")
+    logger.log("Take screenshot")
 
     printScreen()
         .then((message) => {
             response.send({message})
         })
         .catch((error) => {
-            console.error(error.message)
+            logger.error(error.message)
             response.status(500).send({error: "Something goes wrong!", details: error.message})
         })
 })
 
 app.get("/", (request, response) => {
-    console.log("Send screenshot")
+    logger.log("Send screenshot")
 
     try {
         accessSync(SCREENSHOT_PATH, constants.F_OK)
         response.sendFile(SCREENSHOT_PATH, {root: __dirname})
         return
     } catch (error) {
-        console.error(error.message)
+        logger.error(error.message)
     }
 
     printScreen()
@@ -61,12 +72,34 @@ app.get("/", (request, response) => {
             response.sendFile(SCREENSHOT_PATH, {root: __dirname})
         })
         .catch((error) => {
-            console.error(error.message)
+            logger.error(error.message)
             response.status(500).send({error: "Something goes wrong!", details: error.message})
         })
 })
 
+const isNaturalNumber = (number) =>
+    typeof number === "number" && Number.isFinite(number) && Number.isInteger(number) && number >= 0 && number < 24
+
 app.listen(process.env.PORT || PORT, () => {
-    console.log(`Current version: ${process.env.npm_package_version}`)
-    console.log(`Express listening at port: ${PORT}`)
+    const auto_hours = process.env.AUTO_HOURS.split(" ")
+        .map((number) => +number)
+        .filter(isNaturalNumber)
+        .filter((value, index, self) => self.indexOf(value) === index)
+        .sort((a, b) => a - b)
+
+    if (auto_hours.length) {
+        logger.log(`Update screenshot at this hour(s):`, auto_hours)
+
+        cron.schedule(`0 0 ${auto_hours.join(",")} * * *`, async () => {
+            logger.log("Automatically take screenshot", new Date().toString())
+            try {
+                await printScreen()
+            } catch (error) {
+                logger.error(error.message)
+            }
+        })
+    }
+
+    logger.log(`Current version: ${process.env.npm_package_version}`)
+    logger.log(`Express listening at port: ${PORT}`)
 })
